ab56a4a2ca0e6aa38eb576c6d9d5427f249c9fc2
rameshroy83 <rameshroy83@gmail.com>
2020-10-20 03:11:41 -0400

    commit1

---
+++ Lab005.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''
+In this lesson we will learn about variables, variables are memory location which some data is stored.
+Python allows you to store multiple variable types like string, integer, float, boolean etc.
+You do not need to define the variable type and Python kind of auto detects it when the variables are define.
+
+'''
+
+a = 'Ramesh'
+print(type(a))
+
+b = 12345
+print(type(b))
+
+c = 123.45
+print(type(c))
+
+d = True
+print(type(d))
