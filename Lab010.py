a4b2636da96a7324cc7b75ca9462480a4ca012e7
rameshroy83 <rameshroy83@gmail.com>
2020-10-24 08:17:36 -0400

    commit1

---
+++ Lab010.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''
+In this program we will compare two numbers
+'''
+
+a = input("Please enter the first number:" )
+b = input("Please enter the second number:" )
+print(f"Is the value of {a} > {b}:", int(a)>int(b))
---
+++ Lab011.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''
+In this program we will calculate average of two numbers
+And Square of a number
+'''
+
+a = input("Enter the First Number:" )
+b = input("Enter the Second Number:" )
+print(f"The average of {a} and {b} is :", (int(a) + int(b))/2)
+
+print(f"The square of {a} is:", int(a)*int(a))
