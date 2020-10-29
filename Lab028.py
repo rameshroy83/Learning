0c39caa639b250a52c222216b8cfe2543b4d9dbd
rameshroy83 <rameshroy83@gmail.com>
2020-10-29 11:25:40 -0400

    commit

---
+++ Lab028.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+
+The program to solve practice set 7 pattern programming
+'''
+
+num1 = int(input("Please enter a number: "))
+
+for num1 in range(1,num1+1):
+    print(num1*"*")
+
+
