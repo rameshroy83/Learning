5f1f7c0038e0f416b3a5f5c6885327197555319d
rameshroy83 <rameshroy83@gmail.com>
2020-10-31 10:46:34 -0400

    commit1

--- Lab030.py
+++ Lab030.py
@@ -6,3 +6,16 @@ This program is for the practice set chapter 8
 
 '''
 
+
+
+def greatest (num1, num2, num3):
+    list1 = [num1, num2, num3]
+    list1.sort()
+    print(f"The greatest number is : ",list1[2])
+
+
+num1 = int(input("Please enter the 1st number : "))
+num2 = int(input("Please enter the 2st number : "))
+num3 = int(input("Please enter the 3st number : "))
+
+greatest(num1,num2,num3)
