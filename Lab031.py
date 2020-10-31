d47d10da47ea272621cb5a6fb2fe63fb22bf4675
rameshroy83 <rameshroy83@gmail.com>
2020-10-31 10:57:48 -0400

    commit1

---
+++ Lab031.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+Progam for practice set 8 question 2
+
+'''
+
+def ctof (temp):
+    print(f"The temperate {temp} C in Fereinhiet is :",((temp*(9/5))+32))
+
+
+temp = int(input("Please enter the temperate in C : "))
+ctof(temp)
+
+
