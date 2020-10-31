0dcf2f2333141470536624e8f838351fa97cfd11
rameshroy83 <rameshroy83@gmail.com>
2020-10-31 15:34:12 -0400

    commit

---
+++ Lab032.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import inspect
+
+'''
+Program to avoid print next line in the print function
+'''
+
+print("Hello Ramesh ", end ="")
+print("Namste ji")
+
+print(dir(print()))
+
+
