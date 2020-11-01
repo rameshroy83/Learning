b03ea37a8b7b1d8c4648d59ff2a97331d5023cdf
rameshroy83 <rameshroy83@gmail.com>
2020-11-01 06:17:36 -0500

    commit

--- Lab036.py
+++ Lab036.py
@@ -5,13 +5,26 @@ This program is to program the practice set of chapter 9
 
 '''
 
-def game():
-    return(10)
+def game(score):
+    print("Your Score for the Game is : ",score) 
+    return(score)
 
 # The function game is returning a score, now we have to built a logic if the score is overwitten or not.
 
-f = open('Hiscore.txt','w')
-score = f.write('10')
-f.close()
+
+def scoreupdate(score):
+    f = open('Hiscore.txt','r')
+    Hiscore = int(f.read())
+    if int(Hiscore) > int(score) :
+        None
+    elif int(Hiscore) <= int(score) :
+        f = open('Hiscore.txt','w')
+        f.write(str(score))
+
+
+score = int(input("Lets Play and Game and Enter Your Score : "))
+game(score)
+scoreupdate(score)
+    
 
 
