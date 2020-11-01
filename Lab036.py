fa7be9ade37aa70acedcbd1222e3bc006af6e5fb
rameshroy83 <rameshroy83@gmail.com>
2020-11-01 00:18:40 -0400

    commit

---
+++ Lab036.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+This program is to program the practice set of chapter 9
+
+'''
+
+def game():
+    return(10)
+
+# The function game is returning a score, now we have to built a logic if the score is overwitten or not.
+
+f = open('Hiscore.txt','w')
+score = f.write('10')
+f.close()
+
+
